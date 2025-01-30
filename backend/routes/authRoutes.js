5cc9226eebcf848a52ea029c86cf93341bf957b0
Hargun <hargun199920@gmail.com>
2025-01-30 15:11:38 +0530

    added get profile route

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,10 +1,11 @@
 import express from"express"
 import { loginController, logoutController, signupController,refreshController,getProfile } from "../controllers/authController.js";
+import { protectRoute } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 router.post('/signup',signupController)
 router.post('/login',loginController)
 router.post('/logout',logoutController)
 router.post('/refresh-token',refreshController)
-router.get('/profile',getProfile)
+router.get('/profile',protectRoute,getProfile)
 export default router
